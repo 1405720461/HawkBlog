8fbd231cf756b2fde4355e61054c70b2aceac2a3
shi <1613285977@qq.com>
2023-09-04 20:19:43 +0800

    初始化

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import router from "@/router";
+import axios from "./utils/axios.ts";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import { createPinia } from "pinia";
+const pinia = createPinia();
+
+const app = createApp(App);
+app.use(router).use(ElementPlus).use(pinia).mount("#app");
+app.config.globalProperties.$axios = axios;
